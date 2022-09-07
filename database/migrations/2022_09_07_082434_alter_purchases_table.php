0610eec44c9fb62419618e25b060488a11a2dcaa
talha <talha@invotyx.com>
2022-09-07 14:09:46 +0500

    done with purchases migration

--- database/migrations/2022_09_07_082434_alter_purchases_table.php
+++ database/migrations/2022_09_07_082434_alter_purchases_table.php
@@ -14,7 +14,21 @@ class AlterPurchasesTable extends Migration
     public function up()
     {
         Schema::table('purchases', function (Blueprint $table) {
-           $table->unsignedBigInteger('legacy_article_Id')->nullable();
+               $table->integer('legacy_article_id')->nullable();
+               $table->string('actual_price')->nullable();
+               $table->string('sell_price')->nullable();
+               $table->integer('manufacture_id')->nullable();
+               $table->integer('model_id')->nullable();
+               $table->string('engine_details')->nullable();
+               $table->integer('eng_linkage_target_id')->nullable();
+               $table->integer('assembly_group_node_id')->nullable();
+
+               $table->index('legacy_article_id');
+               $table->index('manufacture_id');
+               $table->index('model_id');
+               $table->index('eng_linkage_target_id');
+               $table->index('assembly_group_node_id');
+               $table->index('supplier_id');
            
         });
     }
