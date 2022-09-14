9f2c72878c86a8020c84fdd6cf5dc65184ad82b4
afzaal-ahmed0036 <afzaal.ahmed0036@gmail.com>
2022-09-14 10:53:11 +0500

    Model Create view

--- resources/views/model_series/create.blade.php
+++ resources/views/model_series/create.blade.php
@@ -12,7 +12,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header d-flex align-items-center">
-                        <h4>{{trans('file.Add Manufacturer')}}</h4>
+                        <h4>{{trans('file.Add Model')}}</h4>
                     </div>
                     <div class="card-body">
                         <p class="italic"><small>{{trans('file.The field labels marked with * are required input fields')}}.</small></p>
@@ -32,19 +32,44 @@
                                 </ul>
                             </div>
                         @endif
-                        <form action="{{ route('manufacturer.store') }}" method="post" enctype="multipart/form-data">
+                        <form action="{{ route('modelseries.store') }}" method="post" enctype="multipart/form-data">
                             @csrf
                             <div class="row">
                                 <div class="col-12">
                                     <div class="other_data"></div>
                                     <div class="row">
-                                        <div class="col-6">
+                                        <div class="col-4">
                                             <div class="form-group">
-                                                <strong>Manufacturer Name: *</strong>
-                                                <input type="text" name="manuName" class="form-control" required value="{{old('manuName')}}">
+                                                <strong>Model Name: *</strong>
+                                                <input type="text" name="modelname" class="form-control" required value="{{old('modelname')}}">
                                             </div>
                                         </div>
-                                        <div class="col-6">
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <strong>Construction Year From: *</strong>
+                                                <input type="text" name="yearOfConstrFrom" class="form-control" required value="{{old('yearOfConstrFrom')}}" placeholder="year/month">
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <strong>Construction Year To: *</strong>
+                                                <input type="text" name="yearOfConstrTo" class="form-control" required value="{{old('yearOfConstrTo')}}" placeholder="year/month">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <strong>Manufacturer *</strong>
+                                                <select name="manuId" id="" class="form-control" required>
+                                                    <option value="" selected disabled>--Select One--</option>
+                                                    @foreach($manufacturers as $manufacturer)
+                                                    <option value="{{$manufacturer->manuId}}">{{$manufacturer->manuName}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
                                             <div class="form-group">
                                                 <strong>Linkage Target Type: *</strong>
                                                 <select name="linkingTargetType" id="" class="form-control" required>
