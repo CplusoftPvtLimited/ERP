5e8e4fbf23ef89aa4012971c8f451c45696be2a4
afzaal-ahmed0036 <afzaal.ahmed0036@gmail.com>
2022-10-24 15:54:47 +0500

    Chassis

--- app/Models/Ambrand.php
+++ app/Models/Ambrand.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AmBrand extends Model
 {
+    
     use HasFactory , SoftDeletes;
     protected $table = "ambrand";
     protected $fillable = [
